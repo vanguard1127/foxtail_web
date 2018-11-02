fe540726dfa5390d7721b3da7b482219af8fc116
Cecil <cj@lovapp.co>
2018-11-01 21:06:34 -0700

    send message working

--- src/components/Chat/Chatroom.js
+++ src/components/Chat/Chatroom.js
@@ -1,80 +1,113 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import { Query, Mutation } from "react-apollo";
+import { OPEN_CHAT, SEND_MESSAGE } from "../../queries";
 import { List, Form, Input, Button } from "antd";
 
 import Message from "./Message.js";
 
 class Chatroom extends Component {
+  state = { loading: true };
   componentDidMount() {
-    if (!this.props.loading) {
-      this.scrollToBot();
-    }
+    this.scrollToBot();
   }
 
   componentDidUpdate() {
-    if (!this.props.loading) {
-      this.scrollToBot();
-    }
+    this.scrollToBot();
   }
 
   scrollToBot() {
-    ReactDOM.findDOMNode(this.refs.chats).scrollTop = ReactDOM.findDOMNode(
-      this.refs.chats
-    ).scrollHeight;
-  }
-
-  submitMessage(e) {
-    e.preventDefault();
-
-    this.setState(
-      {
-        chats: this.state.chats.concat([
-          {
-            username: "Kevin Hsu",
-            content: <p>{ReactDOM.findDOMNode(this.refs.msg).value}</p>,
-            img: "http://i.imgur.com/Tj5DGiO.jpg"
-          }
-        ])
-      },
-      () => {
-        ReactDOM.findDOMNode(this.refs.msg).value = "";
-      }
-    );
+    // if (this.refs.chats) {
+    //   ReactDOM.findDOMNode(this.refs.chats).scrollTop = ReactDOM.findDOMNode(
+    //     this.refs.chats
+    //   ).scrollHeight;
+    // }
   }
 
   render() {
-    const { style, messages, loading } = this.props;
-    const screen = !loading ? (
-      <List className="chats" ref="chats">
-        {messages.map(message => (
-          <Message key={message.id} message={message} />
-        ))}
-      </List>
-    ) : (
-      <div style={{ height: "100%" }}>Loading</div>
-    );
+    const { style, chatID } = this.props;
     return (
       <div className="chatroom" style={style}>
         <h3>Foxtail</h3>
-        {screen}
-        <InputForm />
+        <Query
+          query={OPEN_CHAT}
+          variables={{ chatID }}
+          fetchPolicy="cache-and-network"
+        >
+          {({ data, loading, error, fetchMore }) => {
+            if (loading) {
+              return <div style={{ height: "100%" }}>Loading</div>;
+            }
+
+            if (error) {
+              return <div>Error: {error.message}</div>;
+            }
+
+            return (
+              <List className="chats" ref="chats">
+                {data.openChat.messages.map(message => (
+                  <Message key={message.id} message={message} />
+                ))}
+              </List>
+            );
+          }}
+        </Query>
+        <InputForm chatID={chatID} />
       </div>
     );
   }
 }
 
 class InputFormTemplate extends Component {
+  submitMessage(e, sendMessage) {
+    e.preventDefault();
+
+    this.props.form.validateFields((err, fieldsValue) => {
+      if (err) {
+        return;
+      }
+
+      console.log("Received values of form: ", fieldsValue);
+
+      sendMessage()
+        .then(({ data }) => {
+          console.log(data);
+        })
+        .catch(e => console.log(e.message));
+    });
+  }
+
   render() {
+    const { chatID } = this.props;
     const { getFieldDecorator } = this.props.form;
     return (
-      <Form className="input" onSubmit={e => this.submitMessage(e)}>
-        <Form.Item style={{ marginBottom: "0px" }}>
-          {getFieldDecorator("text")(<Input type="text" />)}
-        </Form.Item>
-        <Form.Item style={{ marginBottom: "0px" }}>
-          <Button type="submit">Send</Button>
-        </Form.Item>
-      </Form>
+      <Mutation
+        mutation={SEND_MESSAGE}
+        variables={{
+          chatID,
+          text: this.props.form.getFieldValue("text")
+        }}
+        fetchPolicy="cache-and-network"
+        refetchQueries={() => [
+          {
+            query: OPEN_CHAT,
+            variables: { chatID }
+          }
+        ]}
+      >
+        {(sendMessage, { data, loading, error }) => (
+          <Form className="input">
+            <Form.Item style={{ marginBottom: "0px" }}>
+              {getFieldDecorator("text")(<Input type="text" />)}
+            </Form.Item>
+            <Button
+              type="submit"
+              onClick={e => this.submitMessage(e, sendMessage)}
+            >
+              Send
+            </Button>
+          </Form>
+        )}
+      </Mutation>
     );
   }
 }
