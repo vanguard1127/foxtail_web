54a6c308d6dc7b6b02f9819b9cde657d53786f3c
sanjay <sanjaykumbhar22@gmail.com>
2019-03-26 01:25:47 +0530

    fixed css issue with carousel slider [upcoming events]

--- src/components/SearchEvents/EventsList.js
+++ src/components/SearchEvents/EventsList.js
@@ -35,43 +35,41 @@ class EventsList extends Component {
       <div className="events-card-content">
         <div className="container">
           <div className="col-md-12">
-            <div className="row">
+            <div className="row" id="lightgallery" ref={this.onLightGallery}>
               <div className="col-md-12">
                 <span className="head">{t("upcomingevent")}</span>
               </div>
-              <div id="lightgallery" ref={this.onLightGallery}>
-                <OwlCarousel
-                  nav
-                  autoplay
-                  lazyLoad
-                  margin={30}
-                  navText={[
-                    '<i class="icon-left-open">',
-                    '<i class="icon-right-open">'
-                  ]}
-                  className="owl-carousel slider-content"
-                  autoplayTimeout={2400}
-                  responsive={{
-                    0: {
-                      items: 1,
-                      margin: 15
-                    },
-                    992: {
-                      items: 2,
-                      margin: 15
-                    }
-                  }}
-                >
-                  {events.map(event => (
-                    <EventCard
-                      key={Math.random()}
-                      event={event}
-                      t={t}
-                      dayjs={dayjs}
-                    />
-                  ))}
-                </OwlCarousel>
-              </div>
+              <OwlCarousel
+                nav
+                autoplay
+                lazyLoad
+                margin={30}
+                navText={[
+                  '<i class="icon-left-open">',
+                  '<i class="icon-right-open">'
+                ]}
+                className="owl-carousel slider-content"
+                autoplayTimeout={2400}
+                responsive={{
+                  0: {
+                    items: 1,
+                    margin: 15
+                  },
+                  992: {
+                    items: 2,
+                    margin: 15
+                  }
+                }}
+              >
+                {events.map(event => (
+                  <EventCard
+                    key={Math.random()}
+                    event={event}
+                    t={t}
+                    dayjs={dayjs}
+                  />
+                ))}
+              </OwlCarousel>
               <Waypoint
                 onEnter={({ previousPosition }) => handleEnd(previousPosition)}
               />
