4b3db4d926c65fd56809d4662d9319e789507da4
sanjay <sanjaykumbhar22@gmail.com>
2019-03-30 14:45:21 +0530

    fixed desires display issue and transformed the data

--- src/components/Profile/DesiresMobile.js
+++ src/components/Profile/DesiresMobile.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import _ from "lodash";
+
 class DesiresMobile extends Component {
   shouldComponentUpdate() {
     return false;
   }
+
   render() {
     const { desires, t, ErrorBoundary } = this.props;
 
@@ -12,11 +15,9 @@ class DesiresMobile extends Component {
           <div className="profile-head">{t("Desires")}</div>
           <ul>
             {desires.reduce(function(result, desire) {
-              if (result.length > 1) {
-                result.push(<li key={desire}>...</li>);
-              } else {
-                result.push(<li key={desire}>{t(desire)}</li>);
-              }
+              result.push(
+                <li key={Math.random()}>{_.capitalize(t(desire))}</li>
+              );
               return result;
             }, [])}
           </ul>
