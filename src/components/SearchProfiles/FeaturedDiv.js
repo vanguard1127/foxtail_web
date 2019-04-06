7a797cb93b4fc233b27bab4db32194aa17f72889
sanjay <sanjaykumbhar22@gmail.com>
2019-04-07 01:12:48 +0530

    fixed carousel issue and arrow buttons design

--- src/components/SearchProfiles/FeaturedDiv.js
+++ src/components/SearchProfiles/FeaturedDiv.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
+import $ from "jquery";
+import "lightgallery";
 import FeaturedCard from "./FeaturedCard";
+
+const configLightGallery = {
+  selector: "a",
+  width: "100%"
+};
 
 class FeaturedDiv extends Component {
   shouldComponentUpdate(nextProps) {
-    if (this.props.featuredProfiles !== nextProps.featuredProfiles) {
+    const { featuredProfiles } = this.props;
+    if (featuredProfiles !== nextProps.featuredProfiles) {
       return true;
     }
     return false;
   }
+
+  onLightGallery = node => {
+    this.lightGallery = node;
+    $(node).lightGallery(configLightGallery);
+  };
+
+  componentWillUnmount() {
+    try {
+      $(this.lightGallery).lightGallery("destroy");
+    } catch (e) {}
+  }
+
   render() {
     const {
       featuredProfiles,
@@ -22,55 +40,57 @@ class FeaturedDiv extends Component {
     } = this.props;
 
     return (
-      <section className="featured-profiles" key="na">
+      <section className="featured-profiles">
         <div className="container">
           <div className="col-md-12">
-            <span className="head">{t("featmems")}</span>
-            <OwlCarousel
-              className="owl-carousel slider"
-              autoplay
-              nav
-              margin={30}
-              dots={false}
-              navText={[
-                '<i className="icon-left-open">',
-                '<i className="icon-right-open">'
-              ]}
-              lazyLoad
-              autoplayTimeout={5000}
-              autoplayHoverPause={false}
-              responsive={{
-                0: {
-                  items: 2,
-                  margin: 15
-                },
-                768: {
-                  items: 3,
-                  margin: 15
-                },
-                992: {
-                  items: 4,
-                  margin: 15
-                },
-                1200: {
-                  items: 4
-                }
-              }}
-            >
-              {featuredProfiles.map(profile => {
-                return (
-                  <FeaturedCard
-                    key={profile.id}
-                    profile={profile}
-                    showMsgModal={showMsgModal}
-                    likeProfile={likeProfile}
-                    t={t}
-                    dayjs={dayjs}
-                    history={history}
-                  />
-                );
-              })}
-            </OwlCarousel>
+            <div className="row" ref={this.onLightGallery}>
+              <span className="head">{t("featmems")}</span>
+              <OwlCarousel
+                className="slider"
+                autoplay
+                nav
+                lazyLoad
+                margin={30}
+                dots={false}
+                navText={[
+                  '<i class="icon-left-open">',
+                  '<i class="icon-right-open">'
+                ]}
+                autoplayTimeout={5000}
+                autoplayHoverPause={false}
+                responsive={{
+                  0: {
+                    items: 2,
+                    margin: 15
+                  },
+                  768: {
+                    items: 3,
+                    margin: 15
+                  },
+                  992: {
+                    items: 4,
+                    margin: 15
+                  },
+                  1200: {
+                    items: 4
+                  }
+                }}
+              >
+                {featuredProfiles.map(profile => {
+                  return (
+                    <FeaturedCard
+                      key={Math.random()}
+                      profile={profile}
+                      showMsgModal={showMsgModal}
+                      likeProfile={likeProfile}
+                      t={t}
+                      dayjs={dayjs}
+                      history={history}
+                    />
+                  );
+                })}
+              </OwlCarousel>
+            </div>
           </div>
         </div>
       </section>
