0e0853656225877bca709c78676bdb0fe0a3c2f5
Nikunj Mavani <nikunjmavani003@hotmail.com>
2019-06-12 17:09:09 +0530

    transition fade on edit canvas

--- src/components/Modals/ImageEditor/index.js
+++ src/components/Modals/ImageEditor/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import EditCanvasImage from "./ImageEditorWindow/EditCanvasImage";
 import { withNamespaces } from "react-i18next";
 import Modal from "../../common/Modal";
+import { Spring } from "react-spring/renderprops";
+
 class ImageEditor extends Component {
   shouldComponentUpdate() {
     return false;
@@ -20,25 +22,31 @@ class ImageEditor extends Component {
     } = this.props;
 
     return (
-      <Modal
-        fullScreen
-        header={t("editphoto")}
-        close={close}
-        ref={el => (this.container = el)}
-      >
-        <ErrorHandler.ErrorBoundary>
-          <EditCanvasImage
-            imageObject={file}
-            setS3PhotoParams={setS3PhotoParams}
-            uploadToS3={uploadToS3}
-            signS3={signS3}
-            handlePhotoListChange={handlePhotoListChange}
-            close={close}
-            t={t}
-            ErrorHandler={ErrorHandler}
-          />
-        </ErrorHandler.ErrorBoundary>
-      </Modal>
+      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {props => (
+          <div style={props}>
+            <Modal
+              fullScreen
+              header={t("editphoto")}
+              close={close}
+              ref={el => (this.container = el)}
+            >
+              <ErrorHandler.ErrorBoundary>
+                <EditCanvasImage
+                  imageObject={file}
+                  setS3PhotoParams={setS3PhotoParams}
+                  uploadToS3={uploadToS3}
+                  signS3={signS3}
+                  handlePhotoListChange={handlePhotoListChange}
+                  close={close}
+                  t={t}
+                  ErrorHandler={ErrorHandler}
+                />
+              </ErrorHandler.ErrorBoundary>
+            </Modal>
+          </div>
+        )}
+      </Spring>
     );
   }
 }
