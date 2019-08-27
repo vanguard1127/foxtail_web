9828358cc44dbc64f0901dcd670c21a5925c4b85
Cecil <cj@lovapp.co>
2019-08-27 00:35:26 -0700

    testpush

--- src/index.js
+++ src/index.js
@@ -53,8 +53,8 @@ Sentry.init({
 ReactGA.initialize("UA-106316956-1");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-//let { httpurl, HTTPSurl, wsurl } = env.production;
-let { httpurl, HTTPSurl, wsurl } = env.local;
+let { httpurl, HTTPSurl, wsurl } = env.production;
+//let { httpurl, HTTPSurl, wsurl } = env.local;
 //let { httpurl, HTTPSurl, wsurl } = env.stage;
 
 const wsLink = new WebSocketLink({
