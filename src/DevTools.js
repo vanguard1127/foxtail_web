c1ece13f831b9cca675244172b93d24193d3949b
Cecil <cj@lovapp.co>
2019-09-18 00:21:56 -0700

    broken inbox list2

---
+++ src/DevTools.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import { LOGIN } from "./queries";
+class DevTools extends Component {
+  state = { phone: "" };
+  //TODO:DELETE THIS PRE LAUNCH
+  handleLogin = login => {
+    login()
+      .then(async ({ data }) => {
+        if (data.login === null) {
+          alert(this.props.t("phoneexist"));
+          return;
+        }
+
+        localStorage.setItem(
+          "token",
+          data.login.find(token => token.access === "auth").token
+        );
+        localStorage.setItem(
+          "refreshToken",
+          data.login.find(token => token.access === "refresh").token
+        );
+        window.location.replace("/members");
+      })
+      .catch(res => {
+        const errors = res.graphQLErrors.map(error => {
+          return error.message;
+        });
+
+        return errors;
+      });
+
+    // }
+  };
+  render() {
+    const { phone } = this.state;
+    return (
+      <div className="form terms">
+        Test Users:
+        <Mutation mutation={LOGIN} variables={{ phone }}>
+          {(login, { loading, error }) => {
+            return (
+              <>
+                <span
+                  onClick={() => {
+                    this.setState({ phone: "1" }, () => {
+                      this.handleLogin(login);
+                    });
+                  }}
+                >
+                  1
+                </span>{" "}
+                <span
+                  onClick={() => {
+                    this.setState({ phone: "2" }, () => {
+                      this.handleLogin(login);
+                    });
+                  }}
+                >
+                  2
+                </span>{" "}
+                <span
+                  href={null}
+                  onClick={() => {
+                    this.setState({ phone: "3" }, () => {
+                      this.handleLogin(login);
+                    });
+                  }}
+                >
+                  3
+                </span>{" "}
+                <span
+                  onClick={() => {
+                    this.setState({ phone: "4" }, () =>
+                      this.handleLogin(login)
+                    );
+                  }}
+                >
+                  4
+                </span>
+                <span
+                  onClick={() => {
+                    this.setState({ phone: "5" }, () => {
+                      this.handleLogin(login);
+                    });
+                  }}
+                >
+                  5
+                </span>
+              </>
+            );
+          }}
+        </Mutation>
+      </div>
+    );
+  }
+}
+
+export default DevTools;
