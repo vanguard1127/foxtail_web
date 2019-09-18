692ea0e9186c7a89c7e603782151e69797050bf7
Cecil <cj@lovapp.co>
2019-09-18 00:21:42 -0700

    broken inbox list2

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         loader: "babel-loader",
         test: /\.js$/,
-        exclude: /node_modules/
+        exclude: ["/node_modules/", "/src/DevTools.js"]
       },
       {
         test: /\.s?css$/,
