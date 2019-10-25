8301e6f85bdc4fe31ff2d2c5a3748fae15a9342e
Cecil <cj@lovapp.co>
2019-10-24 18:41:29 -0700

    stage7

---
+++ webpack.stage.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+// var ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
+
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "./build"),
+    publicPath: "/"
+  },
+  mode: "production",
+  node: {
+    fs: "empty"
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: [
+          "/node_modules/",
+          "/src/DevTools.js",
+          "/src/components/Landing/Signup_dev.js"
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: ["/src/assets/favicon.ico", "/src/assets/static"],
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        exclude: /node_modules/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(wav|mp3)$/,
+        exclude: /node_modules/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 10000,
+      automaticNameDelimiter: "_"
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Foxtail",
+      template: "src/page-template.hbs",
+      description: "FREE | Private | 18+ Fun",
+      filename: "index.html",
+      favicon: "./src/assets/favicon.ico",
+      manifest: "./src/assets/manifest.json"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.[contenthash].css"
+    }),
+    new CopyPlugin([
+      { from: "src/assets/locales", to: "locales", toType: "dir" },
+      { from: "src/assets/manifest.json", to: "manifest.json" }
+    ]),
+    new CleanWebpackPlugin(),
+    new Dotenv({ path: "./.env.stage", defaults: true })
+  ]
+};
