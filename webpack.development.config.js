fdfc5c7edfadafb25adcf5f0c6a27476f2a73cc3
Cecil <cj@lovapp.co>
2019-10-24 18:18:42 -0700

    stage3

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -65,7 +65,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Foxtail",
       template: "src/page-template.hbs",
-      description: "New site for alternative relationships",
+      description: "FREE | Private | 18+ Fun",
       filename: "index.html",
       favicon: "./src/assets/favicon.ico",
       manifest: "./src/assets/manifest.json"
